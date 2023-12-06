6583eaf816b3ebae70453080e83b82152a53afba
NikitaSimakov <mishaaniskin@gmail.com>
2023-12-06 18:54:05 +0300

    Homework ✅

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export function FormComponent() {
   const [value, setValue] = useState("");
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
+  const handleChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>): void => {
+    setValue(target.value);
   };
-
-  return (
-    <input type="text" value={value} onChange={handleChange} />
-  );
+  return <input type="text" value={value} onChange={handleChange} />;
 }
+
+// Как типизировать деструктуризированные объекты?
+// Для себя нашел такой лайфхак: если передать в анонимную функцию содержимое handler, то TS
+// сам подсветит нужные типы, но это срабатывает не всегда, как например в этой ситуации.
+// В этой ситуации, я хочу деструктуризировать target из event, в анонимном обработчике по моему "методу"
+// onChange={({ target }) => setValue(target.value)} тип для target: (parameter) target: EventTarget & HTMLInputElement,
+// но если я это переношу в сам handleChange - это не работает.
+// Конкретно в этой ситуации TS согласился на такую же типизацию как и event, но это срабатывает не всегда?
